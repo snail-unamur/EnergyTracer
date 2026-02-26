b0137e04d6ccc1e80a2945c93de9ef079ac8eea8
FloSto-Bash <florian_stormacq@icloud.com>
2026-02-26 15:40:09 +0100

    feat: Implement database connection and query execution in code smell

--- src/python/file_without_code_smell.py
+++ src/python/file_without_code_smell.py
@@ -1,3 +1,26 @@
 # Enter your code without the code smell here.
 
-a = 10 * 10
+def main():
+    import sqlite3
+
+    try :
+        connection = sqlite3.connect('src/python/data/database.sqlite')
+        cursor = connection.cursor()
+
+        REQUEST = """
+            SELECT  EmployeeName, JobTitle, Benefits, TotalPayBenefits, Year
+            FROM Salaries 
+            WHERE BasePay >= 5000
+            LIMIT 10_000
+            """
+        
+        cursor.execute(REQUEST)
+        _ = cursor.fetchall()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        if 'connection' in locals():
+            connection.close()
+
+if __name__ == "__main__":    
+    main()
