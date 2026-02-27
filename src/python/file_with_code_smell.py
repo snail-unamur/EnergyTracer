54c0d3acd4b34d3fb6fe7b511d1b3a76e5283bb5
FloSto-Bash <florian_stormacq@icloud.com>
2026-02-27 19:21:21 +0100

    refactor: removed SQL implementation for simple code example

--- src/python/file_with_code_smell.py
+++ src/python/file_with_code_smell.py
@@ -1,25 +1,3 @@
 # Enter your code with the code smell here.
 
-def main():
-    import sqlite3
-
-    try :
-        connection = sqlite3.connect('src/python/data/database.sqlite')
-        cursor = connection.cursor()
-
-        REQUEST = """
-            SELECT  EmployeeName, JobTitle, Benefits, TotalPayBenefits, Year
-            FROM Salaries 
-            WHERE BasePay >= 5000
-            """
-        
-        cursor.execute(REQUEST)
-        _ = cursor.fetchall()
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    finally:
-        if 'connection' in locals():
-            connection.close()
-
-if __name__ == "__main__":    
-    main()
+a = 10 ** 2
